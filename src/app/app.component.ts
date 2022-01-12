9456adb6e4b8bfc8b9f47c9e25ef8296ef38c7a1
beataj <beata@emc2wave.com>
2022-01-11 21:41:20 -0800

    add showSpinner, loadSpinner()

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,4 +8,9 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'material-demo';
   notifications = 0;
+  showSpinner = false;
+
+  loadSpinner() {
+    this.showSpinner = true;
+  }
 }
