d905cbd92382a3667e412de6225ec02e3cd76c6c
beataj <beata@emc2wave.com>
2022-01-19 19:40:11 -0800

    add filteredOptions

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl } from '@angular/forms';
-import { Observable } from 'rxjs';
-import {map, startWith } from 'rxjs/operators'
+import { Observable, map } from 'rxjs';
+import { startWith } from 'rxjs/operators'
 
 @Component({
   selector: 'app-root',
