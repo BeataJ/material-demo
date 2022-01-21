5d6eae22405c7d5115a3fa20ec5b1f88029c6358
beataj <beata@emc2wave.com>
2022-01-21 15:34:32 -0800

    add {duration: 2000}

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -36,7 +36,7 @@ export class AppComponent implements OnInit{
   constructor(private snackBar: MatSnackBar) {}
 
   openSnackBar(message: any, action: any) {
-    let snackBarRef = this.snackBar.open(message, action);
+    let snackBarRef = this.snackBar.open(message, action, {duration: 2000});
 
     snackBarRef.afterDismissed().subscribe(() => {
       console.log('The snackbar was dismissed')
