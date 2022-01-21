e739002f5928efd0faa7d84c56a391f57855282b
beataj <beata@emc2wave.com>
2022-01-21 15:48:14 -0800

    add CustomSnackBarComponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,7 +9,7 @@ import { MatSnackBar } from '@angular/material/snack-bar';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent implements OnInit{
+export class AppComponent implements OnInit {
   // title = 'material-demo';
   // notifications = 0;
   // showSpinner = false;
@@ -26,25 +26,29 @@ export class AppComponent implements OnInit{
   // filteredOptions: Observable<string[]> | undefined;
   // radioValue: string | undefined;
   minDate = new Date();
-  maxDate = new Date(2022,1,22);
+  maxDate = new Date(2022, 1, 22);
 
-  dateFilter = (date:any) => {
+  dateFilter = (date: any) => {
     const day = date.getDay();
     return day !== 0 && day !== 6;
-  }
+  };
 
   constructor(private snackBar: MatSnackBar) {}
 
   openSnackBar(message: any, action: any) {
-    let snackBarRef = this.snackBar.open(message, action, {duration: 2000});
+    let snackBarRef = this.snackBar.open(message, action, { duration: 2000 });
 
     snackBarRef.afterDismissed().subscribe(() => {
-      console.log('The snackbar was dismissed')
+      console.log('The snackbar was dismissed');
     });
 
     snackBarRef.onAction().subscribe(() => {
       console.log('The snackbar action was triggered!');
-    })
+    });
+  }
+
+  openCustomSnackBar() {
+    this.snackBar.openFromComponent(CustomSnackBarComponent, { duration:2000 });
   }
 
   // loadData() {
@@ -76,10 +80,10 @@ export class AppComponent implements OnInit{
   // }
 
   ngOnInit(): void {
-      // this.filteredOptions = this.myControl.valueChanges.pipe(
-      //   startWith(''),
-      //   map(value => this._filter(value)),
-      // )
+    // this.filteredOptions = this.myControl.valueChanges.pipe(
+    //   startWith(''),
+    //   map(value => this._filter(value)),
+    // )
   }
 
   // private _filter(value: string): string[] {
@@ -96,8 +100,6 @@ export class AppComponent implements OnInit{
 
 @Component({
   selector: 'custom-snackbar',
-  template: `<span style="color: orange">Custom SnackBar</span>`
+  template: `<span style="color: orange">Custom SnackBar</span>`,
 })
-export class CustomSnackBarComponent{
-
-}
+export class CustomSnackBarComponent {}
