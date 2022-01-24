15f8a233e878a2ed9930d6db7d2fd10ed718a4a4
beataj <beata@emc2wave.com>
2022-01-23 19:47:53 -0800

    sorting

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
+import { LiveAnnouncer } from '@angular/cdk/a11y';
 import { FormControl } from '@angular/forms';
 import { Observable, map } from 'rxjs';
 import { startWith } from 'rxjs/operators';
@@ -7,7 +8,7 @@ import { MatDialog } from '@angular/material/dialog';
 import { DialogExampleComponent } from './dialog-example/dialog-example.component';
 import { ConditionalExpr } from '@angular/compiler';
 import { MatTableDataSource } from '@angular/material/table';
-import { MatSort } from '@angular/material/sort';
+import { MatSort, Sort } from '@angular/material/sort';
 
 
 export interface PeriodicElement {
@@ -36,7 +37,7 @@ const ELEMENT_DATA: PeriodicElement[] = [
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent implements OnInit {
+export class AppComponent implements OnInit, AfterViewInit {
   // title = 'material-demo';
   // notifications = 0;
   // showSpinner = false;
@@ -68,7 +69,15 @@ export class AppComponent implements OnInit {
   @ViewChild(MatSort)
   sort!: MatSort;
 
-  constructor(private snackBar: MatSnackBar, public dialog: MatDialog) {}
+  ngAfterViewInit() {
+    this.dataSource.sort = this.sort;
+  }
+
+  constructor(
+    private snackBar: MatSnackBar,
+    public dialog: MatDialog,
+    private _liveAnnouncer: LiveAnnouncer
+  ) {}
 
   logData(row: any) {
     console.log(row);
@@ -127,7 +136,6 @@ export class AppComponent implements OnInit {
   // }
 
   ngOnInit(): void {
-    this.dataSource.sort = this.sort;
     // this.filteredOptions = this.myControl.valueChanges.pipe(
     //   startWith(''),
     //   map(value => this._filter(value)),
@@ -145,11 +153,25 @@ export class AppComponent implements OnInit {
   //   return subject ? subject.name : undefined;
   // }
 
-  applyFilter(event: Event){
-    const filterValue = (event.target as HTMLInputElement).value
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
     this.dataSource.filter = filterValue.trim().toLowerCase();
   }
+
+  announceSortChange(sortState: Sort) {
+    // This example uses English messages. If your application supports
+    // multiple language, you would internationalize these strings.
+    // Furthermore, you can customize the message to add additional
+    // details about the values being sorted.
+    if (sortState.direction) {
+      this._liveAnnouncer.announce(`Sorted ${sortState.direction}ending`);
+    } else {
+      this._liveAnnouncer.announce('Sorting cleared');
+    }
+  }
 }
+
+
 
 @Component({
   selector: 'custom-snackbar',
